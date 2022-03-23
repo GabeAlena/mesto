1e240ac129568e76ac5d32123d2c89f4422a42c3
Gabe Alena <slabsk@mail.ru>
2022-03-23 16:18:11 +0500

    Исправлено 1-ое замечание по классу кард

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,17 +4,38 @@ export class Card {
     constructor(data, cardSelector, handleCardClick) {
         this._name = data.name;
         this._link = data.link;
-        this._pictureTemplate = document.querySelector(cardSelector).content.querySelector('.element');
+        this._cardSelector = cardSelector;
         this._handleCardClick = handleCardClick;
     };
 
-    _deleteCard = () => {
-        this._pictureElement.remove();
-        this._pictureElement = null;
+    _getTemplate() {
+        const pictureElement = document
+          .querySelector(this._cardSelector)
+          .content
+          .querySelector('.element')
+          .cloneNode(true);
+
+          return pictureElement;
+    }
+    
+    createCard() {
+        this._element = this._getTemplate();
+        this._elementLike = this._element.querySelector('.element__like');
+        this._elementTrash = this._element.querySelector('.element__trash');
+        this._elementImage = this._element.querySelector('.element__image');
+        
+        this._fillCard();
+        this._setEventListeners();
+        return this._element;
     };
-     
-    _clickLike = () => {
-        this._elementLike.classList.toggle('element__like_active');
+
+    _fillCard() {
+        const titleElement = this._element.querySelector('.element__title');
+        const imageElement = this._element.querySelector('.element__image');
+    
+        titleElement.textContent = this._name;
+        imageElement.src = this._link;
+        imageElement.alt = this._name;
     };
 
     _setEventListeners() {
@@ -26,26 +47,12 @@ export class Card {
         });
     };
 
-    _fillCard() {
-        const titleElement = this._pictureElement.querySelector('.element__title');
-        const imageElement = this._pictureElement.querySelector('.element__image');
-    
-        titleElement.textContent = this._name;
-        imageElement.src = this._link;
-        imageElement.alt = this._name;
+    _deleteCard = () => {
+        this._element.remove();
+        this._element = null;
     };
-
-    createCard() {
-        //получаем содержимое темплейта обращаясь к его свойству content и клонируем содержимое тега темплейт
-        this._pictureElement = this._pictureTemplate.cloneNode(true);
-
-        this._elementLike = this._pictureElement.querySelector('.element__like');
-        this._elementTrash = this._pictureElement.querySelector('.element__trash');
-        this._elementImage = this._pictureElement.querySelector('.element__image');
-        
-        this._fillCard();
-        this._setEventListeners();
-
-        return this._pictureElement;
+     
+    _clickLike = () => {
+        this._elementLike.classList.toggle('element__like_active');
     };
 }
